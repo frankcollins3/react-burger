c7c9c9e6c48fc4cf68c1301a8b1de436f09db717
Frank Collins <fwc3rd@gmail.com>
2022-08-28 11:38:03 -0400

    [utility.js/data.js] This doesn't belong in main APP.js
    Import the data around which our UI is arranged

---
+++ src/data/List.js
@@ -0,0 +1,42 @@
+const ListComponent = [
+    {name: 'Kaiser Bun', color: 'saddlebrown'},
+    {name: 'Sesame Bun', color: 'sandybrown'},
+    {name: 'Vegan Bun', color: 'peru'},
+    {name: 'Lettuce Bun', color: 'olivedrab'},
+    {name: 'Beef Patty', color: '#3F250B'},
+    {name: 'Soy Patty', color: '#3F250B'},
+    {name: 'Bean Patty', color: '#3F250B'},
+    {name: 'Chicken Patty', color: 'burlywood'},
+    {name: 'Cheese', color: '#ffa600'},
+    {name: 'Lettuce', color: 'lawngreen'},
+    {name: 'Tomato', color: 'tomato'},
+    {name: 'Bacon', color: 'maroon'},
+    {name: 'Onion', color: 'lightyellow'}
+]    
+
+// function ListComponent ()  {
+//     const List = [
+//             {name: 'Kaiser Bun', color: 'saddlebrown'},
+//             {name: 'Sesame Bun', color: 'sandybrown'},
+//             {name: 'Gluten Free Bun', color: 'peru'},
+//             {name: 'Lettuce Wrap Bun', color: 'olivedrab'},
+//             {name: 'Beef Patty', color: '#3F250B'},
+//             {name: 'Soy Patty', color: '#3F250B'},
+//             {name: 'Black Bean Patty', color: '#3F250B'},
+//             {name: 'Chicken Patty', color: 'burlywood'},
+//             {name: 'Lettuce', color: 'lawngreen'},
+//             {name: 'Tomato', color: 'tomato'},
+//             {name: 'Bacon', color: 'maroon'},
+//             {name: 'Onion', color: 'lightyellow'}
+//         ]    
+  
+//     return (
+//         List.map( (listmap) => {
+            
+//           return      <p key={listmap.color}> listmap.name </p>
+//         })
+//     )
+// }
+
+    export default ListComponent
+    // export default ListComponent
